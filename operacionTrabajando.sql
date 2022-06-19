647e80f3c773287b805d0883b826d9cfa5e6ac24
David Achoy <83445439+Davidachoy@users.noreply.github.com>
2022-06-19 16:41:34 -0600

    cambios

--- operacionTrabajando.sql
+++ operacionTrabajando.sql
@@ -4,7 +4,7 @@ SET NOCOUNT ON
 DECLARE @xmlData XML---------------XML
  SET @xmlData = (
 		SELECT *
-		FROM OPENROWSET(BULK 'C:\Users\david\Desktop\ProyectoBD\Datos_Tarea3.xml', SINGLE_BLOB) 
+		FROM OPENROWSET(BULK 'C:\Users\david\Desktop\ProyectoBD\Datos_Tarea3.2.xml', SINGLE_BLOB) 
 		AS xmlData
 		);
 
@@ -64,17 +64,14 @@ DECLARE @NuevosHorarios TABLE (--tablas para las jornadas
 
 DECLARE @FechaItera DATE
 DECLARE @FechaFin DATE
-DECLARE @diaFlag INT
 DECLARE @FechaFinSemana DATE
 DECLARE @RecorrerSemanas DATE
 DECLARE @Semanas INT
-DECLARE @Primera INT = 1
 DECLARE @SecInicio INT
 DECLARE @SecFinal INT
 DECLARE @SecItera INT
 DECLARE @FechaEntrada DATETIME
 DECLARE @FechaSalida DATETIME
-DECLARE @ValorDocumentoIdentidad INT
 DECLARE @Secuencia INT
 DECLARE @empleadoEliminar INT
 DECLARE @tipoEliminar INT
@@ -102,6 +99,20 @@ DECLARE @HorasLaboralesTrabajadas INT
 DECLARE @HoraInicio DATE
 DECLARE @HoraFin DATE
 DECLARE @dialibre DATE
+
+--EMPLEADO
+DECLARE @Nombre VARCHAR(64)
+DECLARE @ValorDocumentoIdentidad INT
+DECLARE @FechaNacimiento DATE
+DECLARE @Username VARCHAR(64)
+DECLARE @Contraseña VARCHAR(64)
+DECLARE @IdTipoDocumentoIdentidad INT
+DECLARE @IdPuesto INT
+DECLARE @IdDepartamento INT
+
+--JORNADA
+DECLARE @IdJornada INT
+
 
 
 
@@ -199,308 +210,341 @@ BEGIN
 
 	-------------------------------------------------------------------------------------------------------------------------------- Insertar empleados
 
+	-- Ingreso de los empleados nuevos 
 
-	INSERT dbo.Empleado([Nombre],
-						[IdTipoIdentificacion],
-						[ValorDocumentoIdentificacion],
-						[IdDepartamento],
-						[IdPuesto],
-						[FechaNacimiento]
-						)
-	SELECT  E.Nombre,
-			E.IdTipoDocumento,
-			E.ValorDocumento,
-			E.IdDepartamento,
-			E.IdPuesto,
-			E.FechaNacimiento
-	FROM @EmpleadosInsertar E
+	If EXISTS(SELECT 1 
+		      FROM @EmpleadosInsertar E
+			  )
+	BEGIN
+		SELECT @SecInicio = MIN(Sec),
+			   @SecFinal = MAX(Sec)
+		FROM @EmpleadosInsertar
 
-	INSERT  dbo.Usuario([IdEmpleado],
-						[Nombre],
-						[Password],
-						[EsAdministrador]
-						)
-	SELECT  E.Id,
-			EXML.Nombre,
-			EXML.Password,
-			0
-	FROM @EmpleadosInsertar EXML
-	INNER JOIN dbo.Empleado E 
-	ON EXML.ValorDocumento = E.ValorDocumentoIdentificacion
+		SET @SecItera = @SecInicio
 
-
----------------------------------------------------------------------------------------------------------------------EmpleadosBorrar
-
-
-	UPDATE Empleado 
-	SET Activo=0 
-	FROM DBO.Empleado E
-	INNER JOIN @EmpleadosBorrar B 
-	ON B.ValorDocumento = E.ValorDocumentoIdentificacion
-	WHERE E.ValorDocumentoIdentificacion = b.ValorDocumento
-	
-
-
--------------------------------------------------------------------------------------------------------------------
-
-
--------------------------------------------------------------------------------------------------------------------NuevosHorarios
-
-
-	INSERT INTO dbo.Jornada([IdEmpleado],
-							[IdSemanaPlanilla],
-							[IdTipoJornada]
-							)
-	SELECT  E.Id AS [IdEmpleado],
-			s.Id AS [IdSemanaPlanilla],
-			N.IdJornada
-	FROM @NuevosHorarios N
-	INNER JOIN Dbo.Empleado E
-	ON N.ValorDocumentoIdentidad = E.ValorDocumentoIdentificacion
-	INNER JOIN DBO.SemanaPlanilla S
-	ON @FechaItera between S.FechaInicio and S.FechaFin
-
-
-
-
-
-
-
-
-
-	-------------------------------------------------------------------------------------------------------------------------------------
-
-	 --Insertar deduccion no obligatorias
-	IF (NOT EXISTS (SELECT * 
-					FROM dbo.FijaNoObligatoria f 
-					INNER JOIN @InsertarDeduccionesEmpleado E
-					ON E.monto = F.Monto
-					WHERE F.Monto = E.monto)
-					)
+		WHILE @SecItera <= @SecFinal
 		BEGIN
-			INSERT INTO dbo.FijaNoObligatoria
-			SELECT  E.monto AS [Monto]
-			FROM @InsertarDeduccionesEmpleado E
+			SELECT @Nombre = E.Nombre,
+				   @ValorDocumentoIdentidad = E.ValorDocumento,
+				   @FechaNacimiento = E.FechaNacimiento,
+				   @IdPuesto = E.IdPuesto,
+				   @IdTipoDocumentoIdentidad = E.IdTipoDocumento,
+				   @IdDepartamento = E.IdDepartamento,
+				   @Username = E.Usuario,
+				   @Contraseña = E.Password
+			FROM @EmpleadosInsertar E
+			WHERE @SecItera = E.Sec
+
+			EXEC [dbo].[sp_InsertarEmpleado]
+				@Nombre,
+				@ValorDocumentoIdentidad,
+				@FechaNacimiento,
+				@IdPuesto,
+				@IdDepartamento,
+				@IdTipoDocumentoIdentidad,
+				@Username,
+				@Contraseña
+
+			SET @SecItera = @SecItera + 1
+		END;	
+	END;
 
 
+	-- Eliminacion de empleados 
+	If EXISTS(SELECT 1 
+		      FROM @EmpleadosBorrar E
+			  )
+	BEGIN
+		SELECT @SecInicio = MIN(Sec), 
+			   @SecFinal = MAX(Sec) 
+		FROM @EmpleadosBorrar;
+
+		SET @SecItera = @SecInicio;
+		WHILE @SecItera <= @SecFinal
+		BEGIN
+			SELECT @ValorDocumentoIdentidad = E.ValorDocumento
+			FROM @EmpleadosBorrar E
+			WHERE @SecItera = E.Sec
+
+			EXEC [dbo].[sp_EliminarEmpleado]
+				@ValorDocumentoIdentidad
+			SET @SecItera = @SecItera + 1
 		END;
+	END;
 
 
 
 
-    INSERT dbo.DeduccionXEmpleado
-	SELECT  E.Id AS [IdEmpleado],
-			I.IdDeduccion AS [IdTipoDeduccion],
-			f.Id AS [IdFijaNoObligatoria],
-			1
-	FROM @InsertarDeduccionesEmpleado I
-	INNER JOIN dbo.Empleado E 
-	ON I.ValorDocumento = E.ValorDocumentoIdentificacion
-	INNER JOIN dbo.FijaNoObligatoria f
-	ON f.Monto = i.monto
 
 
-	 --desasociar (eliminar deducciones) ...
-	SELECT @empleadoEliminar= E.Id, @tipoEliminar = T.Id
-	FROM @EliminarDeduccionesEmpleado D
-	INNER JOIN dbo.Empleado E
-	ON e.ValorDocumentoIdentificacion = D.ValorDocumento
-	INNER JOIN DBO.TipoDeduccion T
-	ON T.Id =D.IdDeduccion 
+-- Asignar Jornada
+
+	If EXISTS(SELECT 1 
+		      FROM @NuevosHorarios N
+			  )
+	BEGIN 
+		SELECT @SecInicio = MIN(Sec), 
+			   @SecFinal = MAX(Sec) 
+		FROM @NuevosHorarios;
+
+		SET @SecItera = @SecInicio;
+
+		WHILE @SecItera <= @SecFinal
+		BEGIN
+			SELECT @ValorDocumentoIdentidad = N.ValorDocumentoIdentidad,
+			       @IdJornada = N.IdJornada
+			FROM @NuevosHorarios N
+			WHERE @SecItera = N.Sec
+
+			EXEC [dbo].[sp_NuevoHorario]
+				@ValorDocumentoIdentidad,
+				@IdJornada
+			SET @SecItera = @SecItera + 1
+		END;
+	END;
+
+
+-- Marcar Asistencia
 
 
 
-	UPDATE dbo.DeduccionXEmpleado
-	set Activo=0
-	where @empleadoEliminar = [IdEmpleado]  AND  @tipoEliminar = [IdTipoDeduccion]
-
-
-	-----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
-	SELECT @lo=Min(A.Sec), @hi=Max(A.Sec)
-	FROM @Asistencias A
+
+--	-------------------------------------------------------------------------------------------------------------------------------------
+
+--	 --Insertar deduccion no obligatorias
+--	IF (NOT EXISTS (SELECT * 
+--					FROM dbo.FijaNoObligatoria f 
+--					INNER JOIN @InsertarDeduccionesEmpleado E
+--					ON E.monto = F.Monto
+--					WHERE F.Monto = E.monto)
+--					)
+--		BEGIN
+--			INSERT INTO dbo.FijaNoObligatoria
+--			SELECT  E.monto AS [Monto]
+--			FROM @InsertarDeduccionesEmpleado E
+
+
+--		END;
+
+
+
+
+--    INSERT dbo.DeduccionXEmpleado
+--	SELECT  E.Id AS [IdEmpleado],
+--			I.IdDeduccion AS [IdTipoDeduccion],
+--			f.Id AS [IdFijaNoObligatoria],
+--			1
+--	FROM @InsertarDeduccionesEmpleado I
+--	INNER JOIN dbo.Empleado E 
+--	ON I.ValorDocumento = E.ValorDocumentoIdentificacion
+--	INNER JOIN dbo.FijaNoObligatoria f
+--	ON f.Monto = i.monto
+
+
+--	 --desasociar (eliminar deducciones) ...
+--	SELECT @empleadoEliminar= E.Id, @tipoEliminar = T.Id
+--	FROM @EliminarDeduccionesEmpleado D
+--	INNER JOIN dbo.Empleado E
+--	ON e.ValorDocumentoIdentificacion = D.ValorDocumento
+--	INNER JOIN DBO.TipoDeduccion T
+--	ON T.Id =D.IdDeduccion 
+
+
+
+--	UPDATE dbo.DeduccionXEmpleado
+--	set Activo=0
+--	where @empleadoEliminar = [IdEmpleado]  AND  @tipoEliminar = [IdTipoDeduccion]
+
+
+--	-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+--	SELECT @lo=Min(A.Sec), @hi=Max(A.Sec)
+--	FROM @Asistencias A
 	 
 
-	WHILE (@lo<=@hi)
-	BEGIN
+--	WHILE (@lo<=@hi)
+--	BEGIN
 
-		SELECT @Entrada=A.Entrada, @Salida=A.Salida, @ValorDocIdentidad= A.ValorDocumento
-		FROM @Asistencias A
-		WHERE A.Sec=@lo
+--		SELECT @Entrada=A.Entrada, @Salida=A.Salida, @ValorDocIdentidad= A.ValorDocumento
+--		FROM @Asistencias A
+--		WHERE A.Sec=@lo
 		
-		SELECT @IdEmpleado = E.Id
-		FROM dbo.Empleado E
-		WHERE E.ValorDocumentoIdentificacion = @ValorDocIdentidad
+--		SELECT @IdEmpleado = E.Id
+--		FROM dbo.Empleado E
+--		WHERE E.ValorDocumentoIdentificacion = @ValorDocIdentidad
 
 
-	----	--Determinar horas ordinarias-------------------------------------------------------------------------------------------------------------
+--	----	--Determinar horas ordinarias-------------------------------------------------------------------------------------------------------------
 
 
-	--	--determinar la jornada de esta semana de ese empleado
+--	--	--determinar la jornada de esta semana de ese empleado
 
 	
 
 
 
-		SELECT @HoraInicioJornada=TJ.HoraInicio, @HoraFinJornada=TJ.HoraFin, @IdJornadaAs = J.Id
-		FROM dbo.SemanaPlanilla PS
-		INNER JOIN dbo.Jornada J 
-		ON PS.Id=J.IdSemanaPlanilla 
-		INNER JOIN dbo.TipoJornada TJ 
-		ON J.IdTipoJornada=TJ.Id
-		WHERE (J.IdEmpleado=@IdEmpleado) AND (@FechaItera BETWEEN PS.FechaInicio AND PS.FechaFin)
+--		SELECT @HoraInicioJornada=TJ.HoraInicio, @HoraFinJornada=TJ.HoraFin, @IdJornadaAs = J.Id
+--		FROM dbo.SemanaPlanilla PS
+--		INNER JOIN dbo.Jornada J 
+--		ON PS.Id=J.IdSemanaPlanilla 
+--		INNER JOIN dbo.TipoJornada TJ 
+--		ON J.IdTipoJornada=TJ.Id
+--		WHERE (J.IdEmpleado=@IdEmpleado) AND (@FechaItera BETWEEN PS.FechaInicio AND PS.FechaFin)
 
 
 		
 	
-	----	--determinar horas ordinarias y horas laborales----------------------------------------------------------------------------------------------------------------
+--	----	--determinar horas ordinarias y horas laborales----------------------------------------------------------------------------------------------------------------
 
-		SET @horasOrdinarias =( DATEDIFF (hh, @Entrada, @Salida ))
-		SET @HorasLaborales = ( DATEDIFF (hh, @HoraInicioJornada, @HoraFinJornada ))
+--		SET @horasOrdinarias =( DATEDIFF (hh, @Entrada, @Salida ))
+--		SET @HorasLaborales = ( DATEDIFF (hh, @HoraInicioJornada, @HoraFinJornada ))
 
 
-	--	--determinar monto ganado por horas ordinarias y horas Extra----------------------------------------------------------------------------------------------
-		SELECT @SalarioXHora = P.SalarioXHora
-		FROM dbo.Puesto P
-		INNER JOIN dbo.empleado E 
-		ON E.IdPuesto = P.Id
-		WHERE E.Id = @IdEmpleado
+--	--	--determinar monto ganado por horas ordinarias y horas Extra----------------------------------------------------------------------------------------------
+--		SELECT @SalarioXHora = P.SalarioXHora
+--		FROM dbo.Puesto P
+--		INNER JOIN dbo.empleado E 
+--		ON E.IdPuesto = P.Id
+--		WHERE E.Id = @IdEmpleado
 
-		SET @MontoGanadoHExtraDoble = 0
-		SET @MontoGanadoHO = 0
-		SET @MontoGanadoHExtra = 0
-		SET @HorasExtraDoble = 0
-		SET @HorasExtra = 0
-		Set @EsJueves = 0
-		SET @EsFinMes = 0
-		SET @MontoGanadoHO = @horasOrdinarias*@SalarioXHora-----------@Monto Ganado Horas Ordinarias
-		SET @HorasLaboralesTrabajadas = @horasOrdinarias--------------total de horas laboradas
+--		SET @MontoGanadoHExtraDoble = 0
+--		SET @MontoGanadoHO = 0
+--		SET @MontoGanadoHExtra = 0
+--		SET @HorasExtraDoble = 0
+--		SET @HorasExtra = 0
+--		Set @EsJueves = 0
+--		SET @EsFinMes = 0
+--		SET @MontoGanadoHO = @horasOrdinarias*@SalarioXHora-----------@Monto Ganado Horas Ordinarias
+--		SET @HorasLaboralesTrabajadas = @horasOrdinarias--------------total de horas laboradas
 
-	----	--------------------------------------------------------------------------------------------------------------------------------------------------Determina si las extras son por 2 0 por 1.5
-		IF (EXISTS (SELECT Fecha 
-					FROM dbo.Feriado f 
-					WHERE @FechaItera = f.Fecha) 
-					OR  DATENAME(DW, @FechaItera) = 'Sunday') 
-					AND (@horasOrdinarias - @HorasLaborales > 0)
-		BEGIN
-			IF @horasOrdinarias - @HorasLaborales > 0
-			BEGIN
-				SET @HorasExtraDoble =  @horasOrdinarias - @HorasLaborales 
-				SET @MontoGanadoHExtraDoble = (@HorasExtraDoble*@SalarioXHora)*2-----------@Monto Ganado Horas EXTRA FERIADO o Domingo
-				SET @HorasLaboralesTrabajadas = @HorasLaboralesTrabajadas - @HorasExtraDoble
-			END
-		END
-		ELSE
-		BEGIN
-			IF @horasOrdinarias - @HorasLaborales > 0
-			BEGIN
-				SET @HorasExtra =  @horasOrdinarias - @HorasLaborales 
-				SET @MontoGanadoHExtra = (@HorasExtra*@SalarioXHora)*1.5-----------@Monto Ganado Horas EXTRA
-				SET @HorasLaboralesTrabajadas = @HorasLaboralesTrabajadas - @HorasExtra
+--	----	--------------------------------------------------------------------------------------------------------------------------------------------------Determina si las extras son por 2 0 por 1.5
+--		IF (EXISTS (SELECT Fecha 
+--					FROM dbo.Feriado f 
+--					WHERE @FechaItera = f.Fecha) 
+--					OR  DATENAME(DW, @FechaItera) = 'Sunday') 
+--					AND (@horasOrdinarias - @HorasLaborales > 0)
+--		BEGIN
+--			IF @horasOrdinarias - @HorasLaborales > 0
+--			BEGIN
+--				SET @HorasExtraDoble =  @horasOrdinarias - @HorasLaborales 
+--				SET @MontoGanadoHExtraDoble = (@HorasExtraDoble*@SalarioXHora)*2-----------@Monto Ganado Horas EXTRA FERIADO o Domingo
+--				SET @HorasLaboralesTrabajadas = @HorasLaboralesTrabajadas - @HorasExtraDoble
+--			END
+--		END
+--		ELSE
+--		BEGIN
+--			IF @horasOrdinarias - @HorasLaborales > 0
+--			BEGIN
+--				SET @HorasExtra =  @horasOrdinarias - @HorasLaborales 
+--				SET @MontoGanadoHExtra = (@HorasExtra*@SalarioXHora)*1.5-----------@Monto Ganado Horas EXTRA
+--				SET @HorasLaboralesTrabajadas = @HorasLaboralesTrabajadas - @HorasExtra
 				
-			END
-		END
+--			END
+--		END
 		
 
-		If  DATENAME(DW, @FechaItera) = 'Thursday'
-		BEGIN
-			SET @EsJueves = 1
+--		If  DATENAME(DW, @FechaItera) = 'Thursday'
+--		BEGIN
+--			SET @EsJueves = 1
 
 			  
-			   --calcular deduccionesObligatorias
+--			   --calcular deduccionesObligatorias
 			  
-			   --calcular deducciones no obligatorias
-		END;
+--			   --calcular deducciones no obligatorias
+--		END;
 
 
 
-		SET @ultimaFecha = DAY(DATEADD(d,1,@FechaItera))--averigua si el dia que sigue es inicio de mes
+--		SET @ultimaFecha = DAY(DATEADD(d,1,@FechaItera))--averigua si el dia que sigue es inicio de mes
 
 
-		If @ultimaFecha = 1
-		BEGIN
-			SET @EsFinMes = 1
-		END;
+--		If @ultimaFecha = 1
+--		BEGIN
+--			SET @EsFinMes = 1
+--		END;
 			
 			
-		--------------------------------------------------------Se empieza la transaction  
-		BEGIN TRANSACTION
-		----	insertar asistencias ----------------------------------------------------------------------no lo he probado
-			INSERT INTO dbo.MarcarAsistencia
-			SELECT @IdJornadaAs AS [IdJornada],
-					@Entrada AS [MarcarInicio],
-					@Salida AS [MarcarFin],
-					@horasOrdinarias,
-					@HorasExtra,
-					@HorasExtraDoble
+--		--------------------------------------------------------Se empieza la transaction  
+--		BEGIN TRANSACTION
+--		----	insertar asistencias ----------------------------------------------------------------------no lo he probado
+--			INSERT INTO dbo.MarcarAsistencia
+--			SELECT @IdJornadaAs AS [IdJornada],
+--					@Entrada AS [MarcarInicio],
+--					@Salida AS [MarcarFin],
+--					@horasOrdinarias,
+--					@HorasExtra,
+--					@HorasExtraDoble
 
 						
-			--insertar movimientoplanilla ()-------------------------------------------------------------Nos faltan tablas por cargar para poder insertar un movimiento
-			INSERT INTO [dbo].[MovPlanilla]
-			SELECT  
-					1,
-					@FechaItera,
-					@MontoGanadoHO,
-					@HorasLaboralesTrabajadas
+--			--insertar movimientoplanilla ()-------------------------------------------------------------Nos faltan tablas por cargar para poder insertar un movimiento
+--			INSERT INTO [dbo].[MovPlanilla]
+--			SELECT  
+--					1,
+--					@FechaItera,
+--					@MontoGanadoHO,
+--					@HorasLaboralesTrabajadas
 					
-			INSERT INTO [dbo].[MovPlanilla]
-			SELECT 
-					2,
-					@FechaItera,
-					@MontoGanadoHExtra,
-					@HorasExtra
+--			INSERT INTO [dbo].[MovPlanilla]
+--			SELECT 
+--					2,
+--					@FechaItera,
+--					@MontoGanadoHExtra,
+--					@HorasExtra
 					
-			WHERE  @HorasExtra>0
+--			WHERE  @HorasExtra>0
 
-			INSERT INTO [dbo].[MovPlanilla]
-			SELECT 
-					3,
-					@FechaItera,
-					@MontoGanadoHExtraDoble,
-					@HorasExtraDoble
+--			INSERT INTO [dbo].[MovPlanilla]
+--			SELECT 
+--					3,
+--					@FechaItera,
+--					@MontoGanadoHExtraDoble,
+--					@HorasExtraDoble
 					
-			WHERE  @HorasExtraDoble>0
+--			WHERE  @HorasExtraDoble>0
 				
 
-			IF @esJueves = 1
-			BEGIN
-				INSERT INTO[dbo].[PlanillaSemXEmpleado]
-				SELECT 0,  
-					(SELECT MAX(Id) FROM [dbo].[SemanaPlanilla]),
-					@IdEmpleado,
-					0
+--			IF @esJueves = 1
+--			BEGIN
+--				INSERT INTO[dbo].[PlanillaSemXEmpleado]
+--				SELECT 0,  
+--					(SELECT MAX(Id) FROM [dbo].[SemanaPlanilla]),
+--					@IdEmpleado,
+--					0
 
 
 
-				--UPDATE [dbo].[PlanillaMesXEmpleado]
-				--SET [SalarioNeto] = @MontoGanadoHExtra + @MontoGanadoHExtraDoble + @MontoGanadoHO
-				--WHERE IdEmpleado = @IdEmpleado AND [IdPlanillaSem] = (SELECT MAX(Id) FROM [dbo].MesPlanilla)
+--				--UPDATE [dbo].[PlanillaMesXEmpleado]
+--				--SET [SalarioNeto] = @MontoGanadoHExtra + @MontoGanadoHExtraDoble + @MontoGanadoHO
+--				--WHERE IdEmpleado = @IdEmpleado AND [IdPlanillaSem] = (SELECT MAX(Id) FROM [dbo].MesPlanilla)
 
 
-				END
+--				END
 				
-			If @EsFinMes  = 1
-			BEGIN
-				   INSERT INTO [dbo].[PlanillaMesXEmpleado]
-				   SELECT  (SELECT MAX(Id) FROM [dbo].MesPlanilla),
-				   0,
-				   0,
-				   @IdEmpleado
-			END
+--			If @EsFinMes  = 1
+--			BEGIN
+--				   INSERT INTO [dbo].[PlanillaMesXEmpleado]
+--				   SELECT  (SELECT MAX(Id) FROM [dbo].MesPlanilla),
+--				   0,
+--				   0,
+--				   @IdEmpleado
+--			END
 				
-			UPDATE [dbo].[PlanillaSemXEmpleado]
-			SET [TotalSalarioBruto] = @MontoGanadoHExtra + @MontoGanadoHExtraDoble + @MontoGanadoHO
-			WHERE IdEmpleado = @IdEmpleado AND [IdPlanillaSem] = (SELECT MAX(Id) FROM [dbo].MesPlanilla)
-		COMMIT TRANSACTION 
+--			UPDATE [dbo].[PlanillaSemXEmpleado]
+--			SET [TotalSalarioBruto] = @MontoGanadoHExtra + @MontoGanadoHExtraDoble + @MontoGanadoHO
+--			WHERE IdEmpleado = @IdEmpleado AND [IdPlanillaSem] = (SELECT MAX(Id) FROM [dbo].MesPlanilla)
+--		COMMIT TRANSACTION 
 			
 			
 			
 		
-		SET @lo = @lo+1
+--		SET @lo = @lo+1
 	
-	END;
+--	END;
 
 	DELETE FROM @EmpleadosInsertar/*Limpia la tabla @EmpleadosInsertar*/
 	DELETE FROM @Asistencias/*Limpia la tabla @Asistencias*/
@@ -510,7 +554,6 @@ BEGIN
 	DELETE FROM @InsertarDeduccionesEmpleado/*Limpia la tabla de @InsertarDeduccionesEmpleado*/
 
 
-	SET @diaFlag = @diaFlag+1
 	SET @FechaItera = DATEADD(DAY,1,@FechaItera)
 
 END;
